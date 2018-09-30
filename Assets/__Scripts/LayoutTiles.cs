a15e758ed3b750b6fae0063ce3d44ef837135e0a
Fabzman <Fabian.Est.Figueroa@gmail.com>
2018-09-30 17:14:15 +1000

    Started Omega Mage Tutorial
    Completed Tile and Tile Layout scripting, level successfully generates on play

---
+++ Assets/__Scripts/LayoutTiles.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileTex
+{
+    public string str;
+    public Texture2D tex;
+}
+
+public class LayoutTiles : MonoBehaviour {
+
+    static public LayoutTiles S;
+
+    public TextAsset roomsText;
+    public string roomNumber;
+    public GameObject tilePrefab;
+    public TileTex[] tileTextures;
+
+    public bool _____;
+
+    public PT_XMLReader roomsXMLR;
+    public PT_XMLHashList roomsXML;
+    public Tile[,] tiles;
+    public Transform tileAnchor;
+
+    private void Awake()
+    {
+        S = this;
+        GameObject tAnc = new GameObject("TileAnchor");
+        tileAnchor = tAnc.transform;
+        roomsXMLR = new PT_XMLReader();
+        roomsXMLR.Parse(roomsText.text);
+        roomsXML = roomsXMLR.xml["xml"][0]["room"];
+
+        BuildRoom(roomNumber);
+    }
+
+    public Texture2D GetTileTex (string tStr)
+    {
+        foreach (TileTex tTex in tileTextures)
+        {
+            if (tTex.str == tStr)
+            {
+                return (tTex.tex);
+            }
+        }
+
+        return null;
+    }
+
+    public void BuildRoom (PT_XMLHashtable room)
+    {
+        string floorTexStr = room.att("floor");
+        string wallTexStr = room.att("wall");
+        string [] roomRows = room.text.Split('\n');
+        for (int i = 0; i < roomRows.Length; i++)
+        {
+            roomRows[i] = roomRows[i].Trim('\t');
+        }
+
+        tiles = new Tile[100, 100];
+        Tile ti;
+        string type, rawType, tileTexStr;
+        GameObject go;
+        int height;
+        float maxY = roomRows.Length - 1;
+
+        for (int y = 0; y < roomRows.Length; y++)
+        {
+            for (int x = 0; x < roomRows[y].Length; x++)
+            {
+                height = 0;
+                tileTexStr = floorTexStr;
+
+                type = rawType = roomRows[y][x].ToString();
+                switch (rawType)
+                {
+                    case " ":
+                    case "_":
+                        continue;
+                    case ".":
+                        break;
+                    case "|":
+                        height = 1;
+                        break;
+                    default:
+                        type = ".";
+                        break;
+                }
+
+                if (type == ".")
+                {
+                    tileTexStr = floorTexStr;
+                }
+
+                else if (type == "|")
+                {
+                    tileTexStr = wallTexStr;
+                }
+
+                go = Instantiate(tilePrefab) as GameObject;
+                ti = go.GetComponent<Tile>();
+                ti.transform.parent = tileAnchor;
+                ti.pos = new Vector3(x, maxY - y, 0);
+                tiles[x, y] = ti;
+
+                ti.type = type;
+                ti.height = height;
+                ti.tex = tileTexStr;
+            }
+        }
+    }
+
+    public void BuildRoom(string rNumStr)
+    {
+        PT_XMLHashtable roomHT = null;
+        for (int i = 0; i < roomsXML.Count; i++)
+        {
+            PT_XMLHashtable ht = roomsXML[i];
+            if (ht.att("num") == rNumStr)
+            {
+                roomHT = ht;
+                break;
+            }
+        }
+        if (roomHT == null)
+        {
+            Utils.tr("ERROR", "LayoutTiles.BuildRoom()",
+                     "Room not found: " + rNumStr);
+            return;
+        }
+        BuildRoom(roomHT);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/__Scripts/Tile.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : PT_MonoBehaviour {
+
+    public string type;
+
+    private string _tex;
+    private int _height = 0;
+    private Vector3 _pos;
+
+        public int height
+    {
+        get { return (_height); }
+        set {
+            _height = value;
+            AdjustHeight();
+        }
+    }
+
+    public string tex
+    {
+        get { return (_tex); }
+        set
+        {
+            _tex = value;
+            name = "tileprefab_" + _tex;
+            Texture2D t2D = LayoutTiles.S.GetTileTex(_tex);
+            if (t2D == null)
+            {
+                Utils.tr("error", "tile.type{set}=", value, "no matching texture2d in layouttiles.s.tiletextures!");
+            }
+
+            else
+            {
+                GetComponent<Renderer>().material.mainTexture = t2D;
+                //renderer.material.maintexture = t2d;
+            }
+        }
+    }
+
+    new public Vector3 pos
+    {
+        get { return (_pos); }
+        set {
+            _pos = value;
+            AdjustHeight();
+        }
+    }
+
+    public void AdjustHeight()
+    {
+        Vector3 vertOffset = Vector3.back * (_height - 0.5f);
+        transform.position = pos + vertOffset;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
